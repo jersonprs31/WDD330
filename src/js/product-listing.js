c6b8891b6310599ad89a90be7dc3a7bd4cbf77c7
Jerson Porras <jersonporras@Jersons-MacBook-Pro.local>
2026-05-23 07:25:54 -0600

    push

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+console.log("Category loaded from URL:", category);
+
+const dataSource = new ProductData();
+const element = document.querySelector(".product-list");
+const listing = new ProductList(category, dataSource, element);
+
+listing.init();
